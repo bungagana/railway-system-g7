21d81286cdf1a23a90205eb09d674802ebda95e4
yamunahmuna <153990747+yamunahmuna@users.noreply.github.com>
2023-12-29 01:00:49 +0800

    Update sidebarCrew.php

--- crew/sidebarCrew.php
+++ crew/sidebarCrew.php
@@ -11,6 +11,7 @@
         <li><a href="../crew/dashCrew.php"><i class="fas fa-home"></i>Dashboard</a></li>
         <li><a href="../crew/scheduleView.php"><i class="fas fa-list"></i>Schedule</a></li>
         <li><a href="../crew/profileCrew.php"><i class="fas fa-address-card"></i>Profile</a></li>
-        <li><a href="../php/index.php" onclick="return confirm('Are you sure you want to log out?')"><i class="fas fa-sign-out-alt"></i>Log Out</a></li>
+        <li><a href="../php/logout.php" onclick="return confirm('Are you sure you want to log out?')"><i class="fas fa-sign-out-alt"></i>Log Out</a></li>
+		
     </ul>
 </div>
