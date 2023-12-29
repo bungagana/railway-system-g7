e2706697c049b3eaf520e55bd089366a9fb663d0
bungagana <bungamagelang57@gmail.com>
2023-12-29 16:33:14 +0700

    pindah file indeks

--- php/connection.php
+++ php/connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "webProject";
+$dbname = "kelasmmc_webProject";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
